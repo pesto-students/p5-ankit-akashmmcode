a9132a0eeca13e5ff4634df6b51243c12a7e4cfd
Akash <akashmm92@gmail.com>
2022-09-20 08:24:20 +0530

    Changed to singlr describe and repaced test() with it()

--- Week-5/Assignment5.4/Assig5.test.js
+++ Week-5/Assignment5.4/Assig5.test.js
@@ -12,19 +12,14 @@ const mathOperations = {
 }
 
 describe("Calculator tests", () => {
-    test('adding 1 + 2 should return 3', () => {
+    it('adding 1 + 2 should return 3', () => {
         expect(mathOperations.addition(1, 2)).toBe(3);
     });
-})
-
-describe("Calculator tests", () => {
-    test('adding 1 + 2 should return 3', () => {
+    it('adding 1 + 2 should return 3', () => {
         expect(mathOperations.subtraction(2, 2)).toBe(0);
     });
-})
 
-describe("Calculator tests", () => {
-    test('adding 1 + 2 should return 3', () => {
+    it('adding 1 + 2 should return 3', () => {
         expect(mathOperations.multiplication(1, 2)).toBe(2);
     });
 })
